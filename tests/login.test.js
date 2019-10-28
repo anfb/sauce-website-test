b37345e5622dc94b14daa50e3f31e7cde318d27b
Adriely Nara <a.freitas.barbosa@avanade.com>
2019-10-28 13:44:26 -0300

    Teste Login Teste login com sucesso modificado para usar multiplos steps.

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,13 +1,26 @@
+const userName = 'standard_user';
+const password = 'secret_sauce';
+   
 module.exports = {
-    'login com sucesso': function(browser){
+
+    'Given user is on login page': (browser) => {
         browser
-            .url("https://www.saucedemo.com/")
-            .waitForElementVisible('.login-box', 5000)
-            .setValue('input[id=user-name]', 'standard_user')
-            .setValue('input[id=password]', 'secret_sauce')
-            .click('.btn_action')
-            .waitForElementVisible('.product_label', 3000)
-            .assert.containsText('.product_label', 'Products')
-            .end();
+        .url("https://www.saucedemo.com/")
+        .waitForElementVisible('.login-box', 3000)
+    },
+
+    'When the user fill your e-mail and password': (browser) => {
+        browser
+        .setValue('input[id=user-name]', userName)
+        .setValue('input[id=password]', password)
+        .click('.btn_action')
+    },
+
+    'Then user should see "Procucts" message': (browser) => {
+        browser
+        .waitForElementVisible('.product_label', 3000)
+        .assert.containsText('.product_label', 'Products')
+        .end();
     }
+
 }
