5dfea9abd3c587d5d44406b009cbf8462adc7d2e
Adriely Nara <a.freitas.barbosa@avanade.com>
2019-10-30 20:19:41 -0300

    Teste Login Implementação de tentativa de login com falha

--- tests/login/fail.test.js
+++ tests/login/fail.test.js
@@ -1,19 +1,64 @@
-const userName = 'standard_user';
-const password = 'sec';
-   
+
+  
 module.exports = {
 
-    'Username or passwork is not correct': (browser) => {
+    'Password is not correct': (browser) => {
         browser
         .url("https://www.saucedemo.com/")
         .waitForElementVisible('.login-box', 3000)
-        .setValue('input[id=user-name]', userName)
-        .setValue('input[id=password]', password)
+        .setValue('input[id=user-name]', 'standard_user')
+        .setValue('input[id=password]', 'se')
         .click('.btn_action')
         .waitForElementVisible('h3[data-test="error"]', 3000)
         .assert.containsText('h3[data-test="error"]', 'Epic sadface: Username and password do not match any user in this service')
+    },
+
+    'Username is not correct': (browser) => {
+        browser
+        .url("https://www.saucedemo.com/")
+        .waitForElementVisible('.login-box', 3000)
+        .setValue('input[id=user-name]', 'stand')
+        .setValue('input[id=password]', 'secret_sauce')
+        .click('.btn_action')
+        .waitForElementVisible('h3[data-test="error"]', 3000)
+        .assert.containsText('h3[data-test="error"]', 'Epic sadface: Username and password do not match any user in this service')
+    },
+
+    'Username is empty': (browser) => {
+        browser
+        .url("https://www.saucedemo.com/")
+        .waitForElementVisible('.login-box', 3000)
+        .setValue('input[id=user-name]', '')
+        .setValue('input[id=password]', 'secret_sauce')
+        .click('.btn_action')
+        .waitForElementVisible('h3[data-test="error"]', 3000)
+        .assert.containsText('h3[data-test="error"]', 'Epic sadface: Username is required')
+    },
+
+    'Password is empty': (browser) => {
+        browser
+        .url("https://www.saucedemo.com/")
+        .waitForElementVisible('.login-box', 3000)
+        .setValue('input[id=user-name]', 'standard_user')
+        .setValue('input[id=password]', '')
+        .click('.btn_action')
+        .waitForElementVisible('h3[data-test="error"]', 3000)
+        .assert.containsText('h3[data-test="error"]', 'Epic sadface: Password is required')
+    },
+
+    'Username and password is empty': (browser) => {
+        browser
+        .url("https://www.saucedemo.com/")
+        .waitForElementVisible('.login-box', 3000)
+        .setValue('input[id=user-name]', '')
+        .setValue('input[id=password]', '')
+        .click('.btn_action')
+        .waitForElementVisible('h3[data-test="error"]', 3000)
+        .assert.containsText('h3[data-test="error"]', 'Epic sadface: Username is required')
         .end();
     }
+
+    
 
     
 }
